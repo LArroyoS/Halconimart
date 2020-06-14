fa570713b5995acb08d24563c31968e98a272320
diegtd <66643309+diegtd@users.noreply.github.com>
2020-06-14 12:16:52 -0500

    Add files via upload

---
+++ vistas/admin/dashboard/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+    $titulo = 'Dashboard';
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <?php include('../modelo/admin/encabezadoAdmin.php'); ?>
+
+    <h2 class='text-center'>¡Bienvenido!</h2>
+
+    <?php include('../modelo/admin/pieAdmin.php'); ?>
+
+</html>
